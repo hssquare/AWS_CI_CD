55408e5ae94d2a5fb49bce3b5f259792cfa6b162
hssquare <shewaleharshal555@gmail.com>
2026-04-10 11:01:03 +0530

    Add files via upload

---
+++ Arithmatic_Operations.py
@@ -0,0 +1,13 @@
+a = int(input("Enter a Number :"))
+b = int(input("Enter a Number :"))
+sum = a+b
+difference = a-b
+product = a*b
+
+quotient = a/b
+print("Sum is :",sum)
+print("Differece is :",difference)
+
+print("Product is :", product)
+
+print("Quotient is :", quotient)
